be6b4c4ac2c7e6142a10474b86675efcb44e2320
portal-immortal <portalingress@gmail.com>
2026-08-13 20:03:49 +0700

    Remove moved projects

--- meeting-link-extractor/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-
-export default defineConfig({
-  site: "https://portal-immortal.github.io",
-  base: "/meeting-link-extractor",
-  output: "static",
-  compressHTML: true,
-});
